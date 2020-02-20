45c9bcb54a6cdecb93da5ff741d418398524fca4
Armadillo <ajknapp2015@gmail.com>
2020-02-20 11:10:28 -0800

    Added db.json file to eventually store notes

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
