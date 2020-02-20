7c17eb2686500578803f47e78c3513dca3c96dd9
Armadillo <ajknapp2015@gmail.com>
2020-02-20 12:40:27 -0800

    created post function to save data from site to the db.json

--- public/index.js
+++ public/index.js
@@ -4,3 +4,10 @@ $('#showNotes').on('click', function(event) {
         $('#note').text(data);
     })
 })
+
+$('#addBtn').on('click', function(event) {
+    event.preventDefault();
+    let newNote = { note: $('#noteInput').val().trim()};
+
+    $.post('/api/notes', newNote);
+})
--- server.js
+++ server.js
@@ -25,6 +25,22 @@ app.get("/api/notes", function(req, res) {
     })
 });
 
+app.post("/api/notes", function(req, res) {
+    let newNote = req.body;
+
+    fs.readFile('db.json', 'utf8', function(err, data) {
+
+        data = JSON.parse(data)
+        data.push(newNote);
+
+        fs.writeFile('db.json', JSON.stringify(data, null, 1), function(err) {
+            if (err) {
+                throw err;
+            }
+        })
+    })
+})
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
