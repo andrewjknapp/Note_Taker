5db5a1da922d9701f1d9700cffabe5f96dd87534
Armadillo <ajknapp2015@gmail.com>
2020-02-23 18:25:46 -0800

    Previous notes are displayed in the correct column on the page

--- public/note.js
+++ public/note.js
@@ -1,9 +1,24 @@
-$('#showNotes').on('click', function(event) {
-    
+
+$( document ).ready(function() {
+    showPrevNotes();
+});
+function showPrevNotes() {
+
     $.get('/api/notes', function(data) {
-        $('#note').text(data);
+        $('#storedNotes').empty();
+        
+        data = JSON.parse(data);
+        for (let i = 0; i < data.length; i++) {
+            let currentNote = `
+            <div class="prevNote">
+                <h4>${data[i].title}<span class="noteDelete"><button index='${i}'>&#215;</button></span></h4>
+                <p>${data[i].note}</p>
+            </div>
+            `
+            $('#storedNotes').prepend(currentNote);
+        }
     })
-})
+}
 
 $('#addBtn').on('click', function(event) {
     event.preventDefault();
@@ -14,12 +29,15 @@ $('#addBtn').on('click', function(event) {
 
     if ($('#noteInput').val().trim() !== '') {
         let newNote = { 
-            title: "New Note",
+            title: $('#noteTitle').val().trim(),
             note: $('#noteInput').val().trim()
         };
+        $('#noteTitle').val('');
         $('#noteInput').val('');
         $.post('/api/notes', newNote);
     }
+
+    showPrevNotes();
 })
 
 $('#delBtn').on('click', function(event) {
